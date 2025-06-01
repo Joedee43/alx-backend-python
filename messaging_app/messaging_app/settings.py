095f1dbebf9a62127fc316bb890d9130fc6b7e2b
joe dee <joedee4311@gmail.com>
2025-06-01 15:11:54 +0300

    added django

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^ne6&=yua0l1eotveg0z7t2fz(5(xl#=to4o3%f36p2k4m2w5q'
+SECRET_KEY = 'django-insecure-95g(6d07pte+w6)n29nt9u#=az_69dj4wxd+7tmz94pm5e4ue('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',  # Django REST Framework
+    'chats',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
+AUTH_USER_MODEL = 'chats.CustomUser'
