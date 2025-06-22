5f494c51d9d56f43515718b2e827ca31a57f8b29
joe dee <joedee4311@gmail.com>
2025-06-22 23:21:56 +0300

    updated

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Message, MessageHistory
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.views.decorators.http import require_POST
 
-def message_history(request, message_id):
-    message = get_object_or_404(Message, pk=message_id)
-    history = message.history.all()
-    return render(request, 'messaging/message_history.html', {
-        'message': message,
-        'history': history
-    })
+User = get_user_model()
+
+@require_POST
+@login_required
+def delete_user(request):
+    user = request.user
+    user.delete()
+    messages.success(request, 'Your account has been successfully deleted.')
+    return redirect('home')  # Replace 'home' with your actual home URL name
