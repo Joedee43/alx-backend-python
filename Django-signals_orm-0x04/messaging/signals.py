6a8b2abe80e065f3d82658127894c27ae13da91e
joe dee <joedee4311@gmail.com>
2025-06-22 23:27:49 +0300

    updated

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -2,6 +2,26 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.utils import timezone
 from .models import Message, MessageHistory
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from messaging.models import Message, MessageHistory, Notification
+
+User = get_user_model()
+@receiver(post_delete, sender=User)
+def cleanup_user_data(sender, instance, **kwargs):
+    """
+    Clean up all user-related data when a user is deleted
+    """
+    # Messages where user is sender or receiver
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+    
+    # Notifications for the user
+    Notification.objects.filter(user=instance).delete()
+    
+    # Message histories where user was the editor
+    MessageHistory.objects.filter(editor=instance).delete()
 
 @receiver(pre_save, sender=Message)
 def log_message_edit(sender, instance, **kwargs):
